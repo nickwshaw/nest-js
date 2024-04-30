2468731cbb17b5df10185522116e5b045aefb067
Nick Shaw <nicholas.shaw@15marketing.co.uk>
2024-04-30 17:34:42 +0100

    Added coffees module

---
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, NotFoundException, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+
+@Controller('coffees')
+export class CoffeesController {
+
+    constructor(private readonly coffeeService: CoffeesService) {}
+
+    @Get()
+    // Example using underlying (Express) response object
+    // findAll(@Res() response) {
+    //     response.status(200).send('This acion returns all coffees');
+    // }
+    findAll(@Query() paginationQuery) {
+        //const { limit, offset } = paginationQuery;
+        return this.coffeeService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        const coffee = this.coffeeService.findOne(id);
+        if (!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found`);
+        }
+        return coffee;
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.GONE)
+    create(@Body() body) {
+        return this.coffeeService.create(body);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return this.coffeeService.update(id, body);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.coffeeService.remove(id);
+    }
+}
